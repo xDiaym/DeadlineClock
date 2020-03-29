705906a87d6354397ede6e9365ac73f5d588fae1
Matthew <r9A784H0$>
2020-03-30 01:01:56 +0300

    Fix for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const express = require('express');
 const path = require('path');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const deadline = getDeadlineUnixTime(config);
 
 const app = express();
